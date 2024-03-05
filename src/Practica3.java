2514f6c24f8ca13c6d11d54f8b32eeda49adeb6d
DAW1-B <DAW1-B@pc>
2024-03-05 17:00:26 +0100

    Segundo Commit

--- src/Practica3.java
+++ src/Practica3.java
@@ -4,6 +4,11 @@ public class Practica3 {
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
 
+		System.out.println("Primer cambio");
+		System.out.println("Segundo cambio");
+		System.out.println("Tercer cambio");
+		
+		String nombre= "Alvaro Trigueros";
 	}
 
 }
